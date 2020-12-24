0a3563fd809ffe569e6c38801211dcef7586bd8a
srichakradhar <srichakra3nsr3@gmail.com>
2020-12-24 16:39:00 -0500

    Add DB and Gantt chart

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -35,6 +36,74 @@ class OfficialProfile(models.Model):
 	county = models.CharField(_("county"), max_length=128, default="Cobb County")
 	country = CountryField(default="US")
 	email_confirmed = models.BooleanField(default=False)
+
+# class Project(models.Model):
+# 	project_id = models.AutoField(primary_key=True)
+# 	title = models.CharField(max_length=100)
+# 	summary = models.CharField(max_length=100)
+# 	description = models.CharField(max_length=100)
+# 	constructor_id = models.CharField(max_length=100)
+# 	project_manager_id = models.CharField(max_length=100)
+# 	risk_manager_id = models.CharField(max_length=100)
+# 	start_date = models.CharField(max_length=100)
+# 	estimated_end_date = models.CharField(max_length=100)
+# 	end_date = models.CharField(max_length=100)
+# 	status = models.CharField(max_length=100)
+# 	site_address = models.CharField(max_length=100)
+# 	site_city = models.CharField(max_length=100)
+# 	site_state = models.CharField(max_length=100)
+# 	site_zip = models.CharField(max_length=100)
+# 	site_country = models.CharField(max_length=100)
+# 	site_phone1 = models.CharField(max_length=100)
+# 	site_phone2 = models.CharField(max_length=100)
+# 	site_fax = models.CharField(max_length=100)
+# 	site_webpage = models.CharField(max_length=100)
+# 	site_email = models.CharField(max_length=100)
+# 	PMO_address = models.CharField(max_length=100)
+# 	PMO_city = models.CharField(max_length=100)
+# 	PMO_state = models.CharField(max_length=100)
+# 	PMO_zip = models.CharField(max_length=100)
+# 	PMO_country = models.CharField(max_length=100)
+# 	PMO_phone1 = models.CharField(max_length=100)
+# 	PMO_phone2 = models.CharField(max_length=100)
+# 	PMO_fax = models.CharField(max_length=100)
+# 	PMO_webpage = models.CharField(max_length=100)
+# 	PMO_email = models.CharField(max_length=100)
+# 	photos = models.FileField(upload_to='uploads/')
+# 	notes = models.CharField(max_length=100)
+# 	attachments = models.FileField(upload_to='uploads/')
+
+
+# class Claim(models.Model):
+# 	claim_id = models.AutoField(primary_key=True)
+# 	project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+# 	title = models.CharField(max_length=100)
+# 	status = models.CharField(max_length=100)
+# 	category = models.CharField(max_length=100)
+# 	priority = models.CharField(max_length=100)
+# 	date_generated = models.DateField(default=datetime.now)
+# 	due_date = models.DateField(default=datetime.now)
+# 	assigned_owner = models.CharField(max_length=100)
+# 	assigned_PM = models.CharField(max_length=100)
+# 	assigned_RM = models.CharField(max_length=100)
+# 	assigned_SubContractor = models.CharField(max_length=100)
+# 	related_claims = models.CharField(max_length=100)
+# 	knowledge_base = models.CharField(max_length=100)
+# 	key_words = models.CharField(max_length=100)
+# 	attachments = models.FileField(upload_to='uploads/')
+# 	notes = models.CharField(max_length=100)
+
+
+# class Message(models.Model):
+# 	message_id = models.AutoField(primary_key=True)
+# 	claim_id = models.ForeignKey(Claim, on_delete=models.CASCADE)
+# 	from_ = models.CharField(max_length=100, db_column='from')
+# 	to = models.CharField(max_length=100)
+# 	body = models.CharField(max_length=65536)
+# 	date_sent = models.CharField(max_length=100)
+# 	status = models.CharField(max_length=100)
+# 	attachments = models.FileField(upload_to='uploads/')
+
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
--- website/settings.py
+++ website/settings.py
@@ -85,6 +85,17 @@ DATABASES = {
     }
 }
 
+# MySQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'DB_NAME',
+#         'USER': 'DB_USER',
+#         'PASSWORD': 'DB_PASSWORD',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
